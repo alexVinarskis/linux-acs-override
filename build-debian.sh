70ee5ff5c27e6addc60030d298ce39bf5e03e220
Alex Vinarskis <alex.vinarskis@gmail.com>
2023-10-15 22:26:16 +0200

    chore: add gitignore, simplify builder

--- build-debian.sh
+++ build-debian.sh
@@ -3,11 +3,10 @@ ACSOPATCH_VER=6.3
 KERNEL_VER=6.3
 sudo apt install build-essential libncurses5-dev fakeroot xz-utils libelf-dev liblz4-tool \
   unzip flex bison bc debhelper rsync libssl-dev:native
-wget -N https://raw.githubusercontent.com/benbaker76/linux-acs-override/main/$ACSOPATCH_VER/acso.patch
 wget -N https://github.com/torvalds/linux/archive/refs/tags/v$KERNEL_VER.zip
 unzip -o v$KERNEL_VER.zip
 cd linux-$KERNEL_VER
-patch -p1 < ../acso.patch
+patch -p1 < ../$ACSOPATCH_VER/acso.patch
 # Disable kernel signing
 sudo make olddefconfig
 sed -i 's|CONFIG_SYSTEM_REVOCATION_KEYS=".*"|CONFIG_SYSTEM_REVOCATION_KEYS=""|g' .config
