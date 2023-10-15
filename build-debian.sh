55a1d8467c256647e2e2d3ce3351d57ff55dbe29
Alex Vinarskis <alex.vinarskis@gmail.com>
2023-10-15 22:42:16 +0200

    fix: apt auto-accept & comments

--- build-debian.sh
+++ build-debian.sh
@@ -3,15 +3,38 @@ set -e
 
 ACSOPATCH_VER=6.3
 KERNEL_VER=6.3
-sudo apt install build-essential libncurses5-dev fakeroot xz-utils libelf-dev liblz4-tool \
-  unzip flex bison bc debhelper rsync libssl-dev:native
+sudo apt install -y --no-install-recommends --no-install-suggests \
+	build-essential \
+	libncurses5-dev \
+	fakeroot \
+	xz-utils \
+	libelf-dev \
+	liblz4-tool \
+ 	unzip \
+	flex \
+	bison \
+	bc \
+	debhelper \
+	rsync \
+	kmod \
+	cpio \
+	zstd \
+	dwarves \
+	libssl-dev:native
+
+# Download kernel sources
 wget -N https://github.com/torvalds/linux/archive/refs/tags/v$KERNEL_VER.zip
 unzip -o -q v$KERNEL_VER.zip
+
+# Aapply acso patch
 cd linux-$KERNEL_VER
 patch -p1 < ../$ACSOPATCH_VER/acso.patch
+
 # Disable kernel signing
 sudo make olddefconfig
 sed -i 's|CONFIG_SYSTEM_REVOCATION_KEYS=".*"|CONFIG_SYSTEM_REVOCATION_KEYS=""|g' .config
 sed -i 's|CONFIG_SYSTEM_TRUSTED_KEYS=".*"|CONFIG_SYSTEM_TRUSTED_KEYS=""|g' .config
 sed -i 's|CONFIG_MODULE_SIG_ALL=y|CONFIG_MODULE_SIG_ALL=n|g' .config
+
+# Build & Package to .deb
 sudo make -j $(nproc) bindeb-pkg LOCALVERSION=-acso KDEB_PKGVERSION=$(make kernelversion)-1
