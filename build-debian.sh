de1a6f6252e6ac635a0237906949aabe71619f8b
Alex Vinarskis <alex.vinarskis@gmail.com>
2023-10-15 22:24:33 +0200

    fix: disable kernel signing

--- build-debian.sh
+++ build-debian.sh
@@ -8,4 +8,9 @@ wget -N https://github.com/torvalds/linux/archive/refs/tags/v$KERNEL_VER.zip
 unzip -o v$KERNEL_VER.zip
 cd linux-$KERNEL_VER
 patch -p1 < ../acso.patch
-sudo make olddefconfig -j $(nproc) bindeb-pkg LOCALVERSION=-acso KDEB_PKGVERSION=$(make kernelversion)-1
+# Disable kernel signing
+sudo make olddefconfig
+sed -i 's|CONFIG_SYSTEM_REVOCATION_KEYS=".*"|CONFIG_SYSTEM_REVOCATION_KEYS=""|g' .config
+sed -i 's|CONFIG_SYSTEM_TRUSTED_KEYS=".*"|CONFIG_SYSTEM_TRUSTED_KEYS=""|g' .config
+sed -i 's|CONFIG_MODULE_SIG_ALL=y|CONFIG_MODULE_SIG_ALL=n|g' .config
+sudo make -j $(nproc) bindeb-pkg LOCALVERSION=-acso KDEB_PKGVERSION=$(make kernelversion)-1
